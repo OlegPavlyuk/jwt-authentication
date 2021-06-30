bd9112b6d50eb645b19ac23ba20855c392e41144
OlegPavlyuk <pavlyuk.o.yu@gmail.com>
2021-06-30 19:09:25 +0300

    add db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const router = require("./router/index");
 
 const PORT = process.env.PORT || 5000;
@@ -17,8 +18,17 @@ app.use((err, req, res, next) => {
   return res.status(500).json({ message: "Something went wrong" });
 });
 
-const start = () => {
-  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
